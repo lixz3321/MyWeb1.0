1d11fbc7a7067b72d93de5690e5521acd5e98997
lixz <wy_lixz@163.com>
2016-12-27 21:32:05 +0800

    3

--- src/com/Dao/Mapper.java
+++ src/com/Dao/Mapper.java
@@ -7,4 +7,5 @@ import com.entity.User;
 public interface Mapper {
 	public User getUser(@Param("id") Integer id);
 	public void addUser(@Param("name") String name,@Param("pass") String pass,@Param("state") Integer state);
+	
 }
